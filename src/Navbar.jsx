09216c8d6d1cc01e4df5c31654d3a927b759afe0
Rushil <rushil13579@gmail.com>
2025-01-04 00:14:04 +0530

    navbar changes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-
-const pathname = window.location.pathname;
+import { Link, useLocation } from 'react-router-dom'
 
 function Navbar() {
+    const location = useLocation();
+    let pathname = location.pathname
+    const handleClick = () => {
+        pathname = location.pathname;
+    }
+
   return (
-    <nav className='flex justify-between items-center p-4 mt-5 mx-auto max-w-5xl border-[2px] rounded-lg border-gray-300'>
+    <nav className='flex justify-between items-center px-4 py-2 my-5 lg:mx-auto mx-6 max-w-5xl border-[2px] rounded-lg border-black'>
         <div className='flex space-x-2'>
-            <img src='../public/window.svg' alt='window' className='w-6' />
-            <img src='../public/arrow.svg' alt='arrow' className='w-6' />
-            <img src='../public/home.svg' alt='home' className='w-6' />
-            <img src='../public/slash.svg' alt='slash' className='w-6' />
+            <img src='/window.svg' alt='window' className='w-6' />
+            <img src='/arrow.svg' alt='arrow' className='w-6' />
+            <Link onClick={handleClick} to='/'><img src='../public/home.svg' alt='home' className='w-6' /></Link>
+            <img src='/slash.svg' alt='slash' className='w-6' />
             {
                 pathname && (
-                    <>
+                    <span className='font-medium font-mono' style={{margin:0}}>
                         {pathname.split('/')}
-                    </>
+                    </span>
                 )
             }
         </div>
-        <div className='flex space-x-4 font-medium text-md'>
-            <Link to='/about'>About</Link>
-            <Link to='/project'>Project</Link>
-            <Link to='/contact'>Contact</Link>
+        <div className='flex space-x-4 font-medium lg:text-lg'>
+            <Link className='hover:bg-slate-200 rounded p-1 lg:p-2 transition' onClick={handleClick} to='/about'>About</Link>
+            <Link className='hover:bg-slate-200 rounded p-1 lg:p-2 transition' onClick={handleClick} to='/project'>Project</Link>
         </div>
     </nav>
   )
