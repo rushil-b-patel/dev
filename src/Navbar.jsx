bcf579f8dfd83c7ae07fb2d7f4c1e5fe2d8d6da9
Rushil <rushil13579@gmail.com>
2025-01-03 21:07:32 +0530

    initialization

---
+++ src/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='flex justify-center bg-black text-white h-8'>
+        <div className='flex justify-between'>
+            <div>
+                1
+            </div>
+            <div>
+                1
+            </div>
+            <div>
+                1
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
