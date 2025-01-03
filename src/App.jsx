685cc7145343f92237f96765ffa29cbc00a9c9ea
Rushil <rushil13579@gmail.com>
2025-01-03 23:33:14 +0530

    navbar struct

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+import About from './About.jsx'
 import Navbar from './Navbar'
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<About />} />
+        <Route path='/project' element={<About />} />
+      </Routes>
+    </>
   )
 }
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const pathname = window.location.pathname;
 
 function Navbar() {
   return (
-    <div className='flex justify-center bg-black text-white h-8'>
-        <div className='flex justify-between'>
-            <div>
-                1
-            </div>
-            <div>
-                1
-            </div>
-            <div>
-                1
-            </div>
+    <nav className='flex justify-between items-center p-4 mt-5 mx-auto max-w-5xl border-[2px] rounded-lg border-gray-300'>
+        <div className='flex space-x-2'>
+            <img src='../public/window.svg' alt='window' className='w-6' />
+            <img src='../public/arrow.svg' alt='arrow' className='w-6' />
+            <img src='../public/home.svg' alt='home' className='w-6' />
+            <img src='../public/slash.svg' alt='slash' className='w-6' />
+            {
+                pathname && (
+                    <>
+                        {pathname.split('/')}
+                    </>
+                )
+            }
         </div>
-    </div>
+        <div className='flex space-x-4 font-medium text-md'>
+            <Link to='/about'>About</Link>
+            <Link to='/project'>Project</Link>
+            <Link to='/contact'>Contact</Link>
+        </div>
+    </nav>
   )
 }
 
