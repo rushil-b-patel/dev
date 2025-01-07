533c75a25d58282f0cb9008f5537128709c855ec
Rushil <rushil13579@gmail.com>
2025-01-07 23:33:47 +0530

    Project Card UI Changed

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,33 +4,45 @@ import {Code} from "./Assets";
 
 const ProjectCard = (props) => {
   return (
-    <div className="group relative lg:w-[600px] rounded-[20px]  p-6 px-8 border-[1px] dark:border-gray-600 transition-all duration-500 overflow-visible hover:shadow-[0_4px_18px_0_rgba(0,0,0,0.25)] dark:hover:shadow-[0_4px_18px_0_rgba(255,255,255,0.1)]">
+    <div className="group relative lg:w-[600px] rounded-lg  p-6 px-8 border-[1px] dark:border-gray-600 transition-all duration-500 overflow-visible hover:shadow-[0_4px_18px_0_rgba(0,0,0,0.25)] dark:hover:shadow-[0_4px_18px_0_rgba(255,255,255,0.1)]">
       <div className="grid text-black dark:text-white">
         <div className="flex justify-between items-center">
           <p className="text-2xl font-medium">{props.title}</p>
           <div className="space-x-5 flex items-center">
-            <button onClick={() => window.open(props.codeLink)}>
-              <Code />
-            </button>
-            <button onClick={() => window.open(props.link)}>
-              <Arrow />
-            </button>
+            {props.codeLink && (
+              <button onClick={() => window.open(props.codeLink)}>
+                <Code />
+              </button>
+            )}
+            {props.liveLink && (
+              <button onClick={() => window.open(props.liveLink)}>
+                <Arrow />
+              </button>
+            )}
           </div>
         </div>
-        <p className="text-gray-600 dark:text-gray-200 my-2 text-base text-justify">{props.description}</p>
-        <div className="font-mono lg:flex justify-between items-center lg:text-base text-sm text-gray-800 dark:text-gray-300">
-          <p className="border dark:border-gray-600 p-1 px-2 rounded-md bg-gray-[#f3f4f6] dark:bg-[#1a1a1a]">{props.startDate} - {props.endDate}</p>
-          <p>
+        <p className="text-gray-600 dark:text-gray-200 my-2 mb-4 text-base text-justify">{props.description}</p>
+        <div className="font-mono space-y-3 justify-between items-center lg:text-base text-sm text-gray-800 dark:text-gray-300">
+          <p className="flex flex-wrap gap-2">
             {props.techStack?.map((tech, index) => (
-              <span key={index} className="ml-2">
-                <span className="border dark:border-gray-600 p-1 rounded-md bg-gray-[#f3f4f6] dark:bg-[#1a1a1a]">{tech}</span>
+              <span key={index} className="border dark:border-gray-600 px-2 py-1 rounded-md bg-gray-[#f3f4f6] dark:bg-[#1a1a1a]">
+                {tech}
               </span>)
             )}
           </p>
+          {props.startDate && (
+            <div className="flex w-max border dark:border-gray-600 p-1 px-2 rounded-md bg-gray-[#f3f4f6] dark:bg-[#1a1a1a]">
+              <p>{props.startDate}</p>
+              <span className="mx-2">-</span>
+              <p>{props.endDate}</p>
+            </div>
+          )}
         </div>
-        <div className="w-full bg-gray-200 rounded-full h-2.5 mt-4 dark:bg-gray-700">
-          <div className="bg-gray-500 h-2.5 rounded-full" style={{width:'45%'}}></div>
-        </div>
+        {props.progress && (
+          <div className="w-full bg-gray-200 rounded-full h-2.5 mt-4 dark:bg-gray-700">
+            <div className="bg-gray-500 h-2.5 rounded-full" style={{width:`${props.progress}%`}}></div>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,15 +8,36 @@ function Projects() {
       <div>
         <div className="flex flex-col space-y-8">
           <ProjectCard 
+              title="BuildFlow" 
+              description='Designed and implemented a scalable and reliable architecture with isolated
+              upload and deploy server for building and deploying the application.' 
+              startDate='Oct 2024' 
+              endDate='Nov 2024' 
+              techStack={['Node JS','Cloudflare','Redis']}
+              codeLink='https://github.com/rushil-b-patel/Build-Flow'
+              progress={100}
+          />
+          <ProjectCard
               title="Whisper" 
               description='A web application allowing students to share opinions and participate in
               discussions without revealing their identities & addressing privacy concerns effectively.' 
               startDate='Sep 2024' 
               endDate='Ongoing' 
-              techStack={['React JS','Node JS','TailwindCSS']}
-              link='https://github.com/rushil-b-patel/Whisper'
+              techStack={['React JS','Node JS','MongoDB', 'T-CSS']}
+              codeLink='https://github.com/rushil-b-patel/Whisper'
+              progress={70}
           />
-          <ProjectCard />
+          <ProjectCard
+              title="EventEase" 
+              description='A web application designed for organizations to showcase upcoming events, 
+              seminars, and lectures with detailed insights.' 
+              startDate='Jan 2024' 
+              endDate='April 2024' 
+              techStack={['Next JS','TypeScript','MongoDB', 'T-CSS']}
+              liveLink='https://eventease-rho.vercel.app/'
+              codeLink='https://github.com/rushil-b-patel/Event-Ease'
+              progress={100}
+          />
         </div>
       </div>
     </div>
