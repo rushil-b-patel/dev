e7d15a87943a0b7d25ae0adb66e2513686a8f402
Rushil <rushil13579@gmail.com>
2025-01-21 22:53:28 +0530

    UI Changes

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -3,6 +3,14 @@ import React from "react";
 const Experience = () => {
   const experiences = [
     {
+      title: "Software Developer Intern",
+      comapny: "Odoo",
+      startDate: "Jan 2024",
+      techStack: ["Javascript", "Python", "XML", "git", "Github"],
+      description:
+        "Collaborated with the development team to create dynamic and responsive user interfaces using React JS. Identified and resolved front-end bugs, ensuring a smooth and error-free user experience. Utilized Git for version control, participating in team code reviews and collaborating with fellow developers.",
+    },
+    {
       title: "Frontend Developer Intern",
       comapny: "Codentic Software",
       startDate: "May 2023",
@@ -17,7 +25,7 @@ const Experience = () => {
     <div className="flex justify-center max-w-3xl lg:mx-auto m-10">
       <ol className="relative border-s border-gray-200 dark:border-gray-700">
         {experiences.map((exp, index) => (
-          <li key={index} className="mb-10 ms-6 p-4 rounded transition duration-300 hover:shadow-[0_4px_10px_0_rgba(0,0,0,0.10)] dark:hover:shadow-[0_4px_10px_0_rgba(255,255,255,0.10)]">
+          <li key={index} className="border dark:border-gray-700 mb-10 ms-6 p-4 rounded transition duration-300 hover:shadow-[0_4px_10px_0_rgba(0,0,0,0.10)] dark:hover:shadow-[0_4px_10px_0_rgba(255,255,255,0.10)]">
             <span className="absolute flex items-center justify-center w-6 h-6 bg-gray-100 rounded-full -start-3 dark:bg-gray-800">
               <svg
                 className="w-2.5 h-2.5 text-black dark:text-white"
@@ -54,7 +62,7 @@ const Experience = () => {
             <div className="font-mono space-y-3 justify-between items-center lg:text-base text-sm text-gray-800 dark:text-gray-300">
             <p className="flex flex-wrap gap-2">
             {exp.techStack?.map((tech, index) => (
-              <span key={index} className="border dark:border-gray-600 px-2 py-1 rounded-md bg-gray-[#f3f4f6] dark:bg-[#1a1a1a]">
+              <span key={index} className="border dark:border-gray-600 px-2 py-1 rounded-md bg-gray-[#f3f4f6] dark:bg-[#1a1a1a] cursor-pointer">
                 {tech}
               </span>)
             )}
